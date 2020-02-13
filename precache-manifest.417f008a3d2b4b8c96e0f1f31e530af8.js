ea60744bc7dc08d372e375936f54b668b3b646d3
Romario Sousa <estreladamanha585@gmail.com>
2020-02-13 08:58:50 -0300

    Updates

---
+++ precache-manifest.417f008a3d2b4b8c96e0f1f31e530af8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8627932f7fbe164ceafbd259a5c447ea",
+    "url": "/calculadora-react2/index.html"
+  },
+  {
+    "revision": "86fe66f2d3a7bb939d36",
+    "url": "/calculadora-react2/static/css/main.9c015d33.chunk.css"
+  },
+  {
+    "revision": "01ef1e2dd8c36b5e1bc2",
+    "url": "/calculadora-react2/static/js/2.540ab3bc.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/calculadora-react2/static/js/2.540ab3bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86fe66f2d3a7bb939d36",
+    "url": "/calculadora-react2/static/js/main.b2de6187.chunk.js"
+  },
+  {
+    "revision": "8124f3fd1fea9b1db740",
+    "url": "/calculadora-react2/static/js/runtime-main.01b797a4.js"
+  },
+  {
+    "revision": "b4618f1f7f4cee0ac09873fcc5a966f9",
+    "url": "/calculadora-react2/static/media/RobotoMono-Regular.b4618f1f.ttf"
+  }
+]);
